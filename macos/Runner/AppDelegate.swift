393bc5abee509d44b4888b146a54b07507fd8a28
Cortana <jian635446559@gmail.com>
2023-07-19 14:33:50 +0800

    add platform channel

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -6,4 +6,10 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+
+  @MainActor
+  override func application(_ sender: NSApplication, openFile filename: String) -> Bool {
+    (mainFlutterWindow as! MainFlutterWindow).currentFile = filename
+    return true
+  }
 }
--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,11 +2,33 @@ import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
+  open var currentFile: String?
+  
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+
+    let batteryChannel = FlutterMethodChannel(
+      name: "samples.flutter.dev/battery",
+      binaryMessenger: flutterViewController.engine.binaryMessenger
+    )
+    batteryChannel.setMethodCallHandler { (call, result) in
+      switch call.method {
+      case "getCurrentFile":
+        if self.currentFile == nil {
+          result(
+            FlutterError(
+              code: "UNAVAILABLE",
+              message: "Current file not available",
+              details: nil))
+        }
+        result(self.currentFile)
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    }
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
